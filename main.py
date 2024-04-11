9bfd5a2ccd520f4e684d28993e7523de1f2c006c
NguyenBinhHCMUS <binhtamnguyen113@gmail.com>
2024-04-11 17:54:08 +0700

    02: Path parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -6,6 +7,27 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
+@app.get("/users")
+async def get_users():
+    return {"message": "Hello world"}
+
+@app.get("/users/{id}")
+async def get_user(id: int):
+    return {"message": "Hello world" + " " + id}
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"message": "Hello world"}
+
 @app.post("/")
 def post():
     return {"message": "Post api" }
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+@app.get("/foods/{food_name}")
+def get_food(food_name: FoodEnum):
+    return {"food_name": food_name}
