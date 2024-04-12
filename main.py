368b547c090a1bc47ff91d4f54679c305428219f
NguyenBinhHCMUS <binhtamnguyen113@gmail.com>
2024-04-12 14:28:28 +0700

    03: Query parameters

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+users = [{"first_name": "Binh", "last_name": "Nguyen", "age": 18}]
 
 @app.get("/", description="Hello world api")
 async def root():
     return {"message": "Hello world"}
 
 @app.get("/users")
-async def get_users():
+async def get_users(page: int = 0, limit: int = 10, sort: Union[str, None] = None):
     return {"message": "Hello world"}
 
 @app.get("/users/{id}")
@@ -23,6 +32,10 @@ async def get_current_user():
 def post():
     return {"message": "Post api" }
 
+@app.put("/users/{id}")
+async def update_user(id: int, user: User):
+    return {"id": id, "user": {"first_name": user.first_name, "last_name": "Nguyen", "age": 18}, "message": "Hello world"}
+
 class FoodEnum(str, Enum):
     fruits = "fruits"
     vegetables = "vegetables"
@@ -31,3 +44,7 @@ class FoodEnum(str, Enum):
 @app.get("/foods/{food_name}")
 def get_food(food_name: FoodEnum):
     return {"food_name": food_name}
+
+
+
+users = [{}]
